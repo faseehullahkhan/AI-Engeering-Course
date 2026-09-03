6fa9f1b429471f46c5b9d5b3c19eb5962786776c
Faseehullah Khan <khanfasihullahkhan@gmail.com>
2026-09-03 22:59:09 +0500

    Add arithmetic, comparison, and logical operators examples

---
+++ Second.py
@@ -0,0 +1,23 @@
+# Operators
+# Arithmetic operators - 7 types add, sub, mul, div, mod, flow division , exponential
+
+a = 5
+b = 20
+print(a + b)
+
+print(b - a)
+print(a * b)
+print(b // a) # the 2 slashes provides us the answer in int not in float
+print(b / a) # this will provide the answer in the float data type
+print(b ** a)
+print(a % b)
+
+# Comparision operators always gives true or false in answer
+
+print(ord("A") > 20)
+
+# Logical-Operator - AND, OR , NOT
+
+print(12 > 10 and 34 ==34)
+print(12 > 10 or 34 != 34)
+print(not 12 == 12)
